d39e24b53a41f117b9964ee3577660b3806b6a90
svillors <tntzator9@gmail.com>
2024-10-28 22:22:17 +0500

    Исправлены main.py и template.html

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
 from collections import defaultdict
+from dotenv import load_dotenv
+import os
 
 
 def declension(years):
@@ -15,12 +17,13 @@ def declension(years):
 
 
 def main():
-    wines = pandas.read_excel('wine3.xlsx', keep_default_na=False)
-    wines_dict = wines.to_dict(orient='records')
-    new_wines = defaultdict(list)
-    for wine in wines_dict:
-        new_wines[wine['Категория']].append(wine)
-    new_wines_dict = dict(new_wines)
+    load_dotenv()
+    wines = pandas.read_excel(os.getenv('WINES_FILE'), keep_default_na=False)
+    wines = wines.to_dict(orient='records')
+    grouped_wines = defaultdict(list)
+    for wine in wines:
+        grouped_wines[wine['Категория']].append(wine)
+    grouped_wines = dict(grouped_wines)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -29,12 +32,11 @@ def main():
 
     template = env.get_template('template.html')
 
-    winery_age = (datetime.datetime.now()
-                  - datetime.datetime(year=1920, month=1, day=1)).days//365
+    winery_age = datetime.datetime.now().year - 1920
 
     rendered_page = template.render(
         time="Уже {} {} с вами".format(winery_age, declension(winery_age)),
-        new_wines_dict=new_wines_dict,
+        grouped_wines=grouped_wines,
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
